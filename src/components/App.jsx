1ae05f44d6a68765650edd5faf8989fe3642f1ac
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2020-12-21 19:40:06 -0500

    fixed the searchbox to have instant search results

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import EmojiList from "./emoji-list";
 import emojipedia from "../emojipedia";
 import SearchBox from "./search-box.component";
@@ -8,18 +8,23 @@ function App() {
 
   const [emojis, setEmojis] = useState(emojipedia);
   const [searchField, setSearchField] = useState('');
-  const handleChange = (e) => {
-    setSearchField(e.target.value); 
-    updateEmojis(searchField);
-  }
 
-  function updateEmojis(searchField){
-    setEmojis( emojis.filter(emoji => 
-      emoji.name.toLowerCase().includes(searchField.toLowerCase())
-    )); 
+  
+
+  function handleChange(e) {
+    setSearchField(e.target.value); 
+  
   }
 
   
+  
+
+  useEffect(() => {
+    const results = emojipedia.filter(emoji => 
+          emoji.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+    setEmojis(results)
+  },[searchField]);
  
 
   return (
@@ -29,7 +34,8 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <SearchBox placeholder="Search Emojis.."
-        handleChange= {handleChange} />
+        handleChange= {handleChange}
+        value={searchField} />
       <dl className="dictionary">
        <EmojiList emojiArr = {emojis}/>
       </dl>
--- src/components/search-box.component.jsx
+++ src/components/search-box.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 
-export const SearchBox =({placeholder, handleChange})=>(  
+export const SearchBox =({value, placeholder, handleChange})=>(  
 <input 
     className="search"
     type="search" 
+    value ={value}
     placeholder={placeholder} 
     onChange={handleChange}  
     />
