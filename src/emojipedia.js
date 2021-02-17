96a3bfc9d3dcc1cead6af573a01b5e402d48e3c9
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-02-17 08:29:37 -0500

    added more emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,152 +1,156 @@
 const emojipedia = [
-    {
-      id: 1,
-      emoji: "💪",
-      name: "Tense Biceps",
-      meaning:
-        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-    },
-    {
-      id: 2,
-      emoji: "🙏",
-      name: "Person With Folded Hands",
-      meaning:
-        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
-    },
-    {
-      id: 3,
-      emoji: "🤣",
-      name: "Rolling On The Floor, Laughing",
-      meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-    },
-    {
-      id: 4,
-      emoji: "😉",
-      name: "Winking Face",
-      meaning:
-        "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."    
-    },
-    {
-      id: 5,
-      emoji: "😇",
-      name: "Smiling Face with Halo",
-      meaning:
-        "A yellow face with smiling eyes, closed smile, and halo, usually blue, overhead. Often represents angels, prayers, and blessings. May also convey angelic behavior, e.g., doing good deeds"    
-    },
-    {
-      id: 6,
-      emoji: "🙃",
-      name: "Upside-Down Face",
-      meaning:
-        "Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness. Its intent can be similar to the bemused Oh well! of 🤷 Person Shrugging."    
-    },
-    {
-      id: 7,
-      emoji: "🤩",
-      name: "Star-Struck",
-      meaning:
-        "A face with a broad, open smile, showing upper teeth on most platforms, with stars for eyes, as if seeing a beloved celebrity. Color of stars widely varies by platform, though commonly gold or red. May express that someone or something is amazing, fascinating, impressive, or exciting."    
-    },
-    {
-      id: 8,
-      emoji: "🥰",
-      name: "Smiling Face with Hearts",
-      meaning:
-        " Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform."    
-    },
-    {
-      id: 9,
-      emoji: "😋",
-      name: "Face Savoring Food",
-      meaning:
-        " Widely used to convey that a food item is delicious. May also express that a person is attractive."    
-    },
-    {
-      id: 10,
-      emoji: "🤑",
-      name: "Money-Mouth Face",
-      meaning:
-        " Used for a wide range of content dealing with money, including: making money, loving wealth, being or feeling rich, and concepts of success and excellence (e.g., on the money)."    
-    },
-    {
-      id: 11,
-      emoji: "🙈",
-      name: "See-No-Evil Monkey",
-      meaning:
-        "The Three Wise Monkeys represent the proverb see no evil, hear no evil, speak no evil, frequently interpreted as a call to discretion or figure for willful ignorance. Often used as a playful way to convey a laughing, disbelieving, or cringing I can’t believe what I’m seeing! or I can’t bear to look!"    
-    },
-    {
-      id: 12,
-      emoji: "🙉",
-      name: "Hear-No-Evil Monkey",
-      meaning:
-        "The Three Wise Monkeys represent the proverb see no evil, hear no evil, speak no evil, frequently interpreted as a call to discretion or figure for willful ignorance. Often used as a playful way to convey I can’t believe what I’m hearing! or, more generally, express astonishment or disbelief."    
-    },
-    {
-      id: 13,
-      emoji: "🙊",
-      name: "Speak-No-Evil Monkey",
-      meaning:
-        "The Three Wise Monkeys represent the proverb see no evil, hear no evil, speak no evil, frequently interpreted as a call to discretion or figure for willful ignorance. Often used as a playful way to convey I can’t believe what I just said!, express a mischievous Oops!, or communicate a secret-keeping I won’t say a word. "    
-    },
-    {
-      id: 14,
-      emoji: "🤗",
-      name: "Hugging Face",
-      meaning:
-        "A yellow face smiling with open hands, as if giving a hug. May be used to offer thanks and support, show love and care, or express warm, positive feelings more generally."    
-    },
-    {
-      id: 15,
-      emoji: "🤪",
-      name: "Zany Face",
-      meaning:
-        "Commonly represents such activities as acting goofy, having fun, and partying as well as various senses of slang terms like crazy, usually in a positive manner. May convey a spirit of wackiness more generally."    
-    },
-    {
-      id: 16,
-      emoji: "🥳",
-      name: "Partying Face",
-      meaning:
-        "A yellow face with a party hat blowing a party horn as confetti floats around its head. Used for celebrating joyous occasions and enjoying good times more generally."    
-    },
-    {
-      id: 17,
-      emoji: "😷",
-      name: "Face with Medical Mask",
-      meaning:
-        "A yellow face wearing a white surgical mask, as used by health workers in hospitals as well as the general public during a pandemic. May be worn to avoid sickness or spreading airborne diseases."    
-    },
-    {
-      id: 18,
-      emoji: "👍",
-      name: "Thumbs Up",
-      meaning:
-        "A thumbs-up gesture indicating approval."    
-    },
-    {
-      id: 19,
-      emoji: "👍",
-      name: "Thumbs Down",
-      meaning:
-        "A thumbs-down gesture indicating disapproval."    
-    },
-    {
-      id: 20,
-      emoji: "😲",
-      name: "Astonished Face",
-      meaning:
-        "A yellow face with open eyes, raised eyebrows, and an open mouth, as if gasping in shock or surprise. May convey a wide range of emotions, including awe, amazement, admiration, disbelief, excitement, or concern."    
-    },
-    {
-      id: 21,
-      emoji: "😒",
-      name: "Unamused Face",
-      meaning:
-        "A yellow face with slightly raised eyebrows, a frown, and eyes looking to the side. May convey a variety of negative emotions, including irritation, displeasure, grumpiness, and skepticism, as if giving the side-eye."    
-    }
-  ];
-  
-  export default emojipedia;
-  
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic.",
+  },
+  {
+    id: 5,
+    emoji: "😇",
+    name: "Smiling Face with Halo",
+    meaning:
+      "A yellow face with smiling eyes, closed smile, and halo, usually blue, overhead. Often represents angels, prayers, and blessings. May also convey angelic behavior, e.g., doing good deeds",
+  },
+  {
+    id: 6,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness. Its intent can be similar to the bemused Oh well! of 🤷 Person Shrugging.",
+  },
+  {
+    id: 7,
+    emoji: "🤩",
+    name: "Star-Struck",
+    meaning:
+      "A face with a broad, open smile, showing upper teeth on most platforms, with stars for eyes, as if seeing a beloved celebrity. Color of stars widely varies by platform, though commonly gold or red. May express that someone or something is amazing, fascinating, impressive, or exciting.",
+  },
+  {
+    id: 8,
+    emoji: "🥰",
+    name: "Smiling Face with Hearts",
+    meaning:
+      " Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform.",
+  },
+  {
+    id: 9,
+    emoji: "😋",
+    name: "Face Savoring Food",
+    meaning:
+      " Widely used to convey that a food item is delicious. May also express that a person is attractive.",
+  },
+  {
+    id: 10,
+    emoji: "🤑",
+    name: "Money-Mouth Face",
+    meaning:
+      " Used for a wide range of content dealing with money, including: making money, loving wealth, being or feeling rich, and concepts of success and excellence (e.g., on the money).",
+  },
+  {
+    id: 11,
+    emoji: "🙈",
+    name: "See-No-Evil Monkey",
+    meaning:
+      "The Three Wise Monkeys represent the proverb see no evil, hear no evil, speak no evil, frequently interpreted as a call to discretion or figure for willful ignorance. Often used as a playful way to convey a laughing, disbelieving, or cringing I can’t believe what I’m seeing! or I can’t bear to look!",
+  },
+  {
+    id: 12,
+    emoji: "🙉",
+    name: "Hear-No-Evil Monkey",
+    meaning:
+      "The Three Wise Monkeys represent the proverb see no evil, hear no evil, speak no evil, frequently interpreted as a call to discretion or figure for willful ignorance. Often used as a playful way to convey I can’t believe what I’m hearing! or, more generally, express astonishment or disbelief.",
+  },
+  {
+    id: 13,
+    emoji: "🙊",
+    name: "Speak-No-Evil Monkey",
+    meaning:
+      "The Three Wise Monkeys represent the proverb see no evil, hear no evil, speak no evil, frequently interpreted as a call to discretion or figure for willful ignorance. Often used as a playful way to convey I can’t believe what I just said!, express a mischievous Oops!, or communicate a secret-keeping I won’t say a word. ",
+  },
+  {
+    id: 14,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "A yellow face smiling with open hands, as if giving a hug. May be used to offer thanks and support, show love and care, or express warm, positive feelings more generally.",
+  },
+  {
+    id: 15,
+    emoji: "🤪",
+    name: "Zany Face",
+    meaning:
+      "Commonly represents such activities as acting goofy, having fun, and partying as well as various senses of slang terms like crazy, usually in a positive manner. May convey a spirit of wackiness more generally.",
+  },
+  {
+    id: 16,
+    emoji: "🥳",
+    name: "Partying Face",
+    meaning:
+      "A yellow face with a party hat blowing a party horn as confetti floats around its head. Used for celebrating joyous occasions and enjoying good times more generally.",
+  },
+  {
+    id: 17,
+    emoji: "😷",
+    name: "Face with Medical Mask",
+    meaning:
+      "A yellow face wearing a white surgical mask, as used by health workers in hospitals as well as the general public during a pandemic. May be worn to avoid sickness or spreading airborne diseases.",
+  },
+  {
+    id: 18,
+    emoji: "👍",
+    name: "Thumbs Up",
+    meaning: "A thumbs-up gesture indicating approval.",
+  },
+  {
+    id: 19,
+    emoji: "👍",
+    name: "Thumbs Down",
+    meaning: "A thumbs-down gesture indicating disapproval.",
+  },
+  {
+    id: 20,
+    emoji: "😲",
+    name: "Astonished Face",
+    meaning:
+      "A yellow face with open eyes, raised eyebrows, and an open mouth, as if gasping in shock or surprise. May convey a wide range of emotions, including awe, amazement, admiration, disbelief, excitement, or concern.",
+  },
+  {
+    id: 21,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning:
+      "A yellow face with slightly raised eyebrows, a frown, and eyes looking to the side. May convey a variety of negative emotions, including irritation, displeasure, grumpiness, and skepticism, as if giving the side-eye.",
+  },
+  {
+    id: 22,
+    emoji: "🤥",
+    name: "Lying face",
+    meaning:
+      "A yellow face with raised eyebrows, enlarged eyes, slight frown, and long nose, indicating it’s telling a lie in the manner of Pinocchio.",
+  },
+];
+
+export default emojipedia;
